f54368bb2788331821a27654b0d8e12cca935d80
Pawel Jezior <paweljfrontend@gmail.com>
2022-03-03 20:31:02 +0100

    rebuild data fetching

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
-import { fetchWrongAnswersToQuestions, fetchCorrectAnswerToQuestions } from "./fetchData/anwersToQuestions";
+// import {
+//   fetchWrongAnswersToQuestions,
+//   fetchCorrectAnswerToQuestions,
+// } from "./fetchData/anwersToQuestions";
 import { fetchBot } from "./fetchData/bot";
-import { fetchQuestions } from "./fetchData/questions";
+import { fetchQuestionsWithAnswers } from "./fetchData/questions";
 import { fetchStory } from "./fetchData/story";
 
 function App() {
@@ -14,102 +17,78 @@ function App() {
   const content = "content";
   const correct = "correct";
   const wrong = "wrong";
-  const numbers = [0,1,2,3,4,5,6,7,8,9,10,11]
-  const kindOfOperationBot = ['welcome', 'content', 'end']
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  const kindOfOperationBot = ["welcome", "content", "end"];
 
-  const [wrongAnswers, setWrongAnswers] = useState([])
-  const [correctAnswer, setCorrectAnswer] = useState([])
+  const [botData, setBotData] = useState()
+  const [questionsData, setQuestionsData] = useState()
+  const [storyData, setStoryData] = useState()
+  //==============================================================================
+  const getBotData = async () => {
+    await fetch(db + bot + fetchSuffix)
+    .then((response) => response.json())
+    .then((response) => {
+      setBotData(response)
+    })
+    .catch((error) => {
+      console.log("The content of the BOT cannot be downloaded.", error);
+    });
+  }
+  //==============================================================================
+  const getQuestionsData = async () => {
+    await fetch(db + questions + fetchSuffix)
+    .then((response) => response.json())
+    .then((response) => {
+      setQuestionsData(response)
+    })
+    .catch((error) => {
+      console.log("The content of the QUESTIONS cannot be downloaded.", error);
+    });
+  }
+  //==============================================================================
+  const getStoryData = async () => {
+    await fetch(db + story + fetchSuffix)
+    .then((response) => response.json())
+    .then((response) => {
+      setStoryData(response)
+    })
+    .catch((error) => {
+      console.log("The content of the STORY cannot be downloaded.", error);
+    });
+  }
+  //==============================================================================
 
-  //current question
-  const [currentQuestion, setCurrentQuestion] = useState("x")
-  //submit button
-  const [submitButtonName, setSubmitButtonName] = useState("START")
-  //1-3 wrong, 4 correct
-  const [radioButton1Name, setRadioButton1Name] = useState("x")
-  const [radioButton2Name, setRadioButton2Name] = useState("x")
-  const [radioButton3Name, setRadioButton3Name] = useState("x")
-  const [radioButton4Name, setRadioButton4Name] = useState("x")
-
-  //chat bot text
-  //zrobic, zeby bylo welcome, nie x
-  const [chatBot, setChatBot] = useState("x")
-
-  //teraz zrobic, zeby story sie pobiera, gdy sie dobrze odpowie i zmieniala i ogolnie te odpowiedzi i pytania i wszystko
-  const [mainStory, setMainStory] = useState("x")
-
-  //pobieranie tresci bota, numer 10 - welcome
-  // fetchBot(db, bot, numbers[10], kindOfOperationBot[0], fetchSuffix);
-  //pobieranie tresci pytania
-  fetchQuestions(db, questions, numbers[0], content, fetchSuffix);
-  //pobieranie odpowiedzi do pytania
   useEffect(() => {
-    setWrongAnswers(fetchWrongAnswersToQuestions(db, questions, numbers[0], wrong, fetchSuffix));
-    setCorrectAnswer(fetchCorrectAnswerToQuestions(db, questions, numbers[0], correct, fetchSuffix));
-  });
-  //pobieranie historii
-  // fetchStory(db, story, numbers[0], content, fetchSuffix);
-  // setMainStory(fetchStory(db, story, numbers[0], content, fetchSuffix))
-
-  const [chatBotNumber,setChatBotNumber] = useState(0)
-  useEffect (()=>{
-    setChatBot(fetchBot(db, bot, numbers[chatBotNumber], kindOfOperationBot[1], fetchSuffix))
-  },[chatBotNumber])
-
-  const [questionNumber, setQuestionNumber] = useState(-1)
-  useEffect (() =>{
-    setCurrentQuestion(fetchQuestions(db, questions, numbers[0], content, fetchSuffix))
-  }, [questionNumber])
+    getBotData()
+    getQuestionsData()
+    getStoryData()
+  },[])
 
 
   const check = () => {
-    if (submitButtonName === "SUBMIT") {
-      //funcje podajace pytania i spraawdzajce odpowiedz oraz zmiana story, doklejanie do poprzedniego
-      
-    } else {
-      setSubmitButtonName("SUBMIT")
-
-      setRadioButton1Name(wrongAnswers[0])
-      setRadioButton2Name(wrongAnswers[1])
-      setRadioButton3Name(wrongAnswers[2])
-      setRadioButton4Name(correctAnswer[0])
-
-      // setCurrentQuestion(fetchQuestions(db, questions, numbers[0], content, fetchSuffix))
-      setQuestionNumber(0)
-      setChatBotNumber(1)
-    
-      // setChatBot(fetchBot(db, bot, numbers[0], kindOfOperationBot[1], fetchSuffix))
-
-
-      //===>>>teraz zrobic, zeby z 'welcome' zmienialo sie na pierwszy tekst bota
-
-      // setMainStory(fetchStory(db, story, numbers[0], content, fetchSuffix))
-      // console.log("mainStory: " + mainStory)
-      //funkcje zmieniajace widocznosc i wyswietlajace pytanie
-    }
+    console.log(botData)
+    console.log(questionsData)
+    console.log(storyData)
   }
-
-  // const getCurrentQuestion = () => {return currentQuestion}
 
   return (
     <div className="app">
       <div className="app__chat">
-        <h3>{chatBot}</h3>
+        <h3>abc</h3>
       </div>
       <div className="app__question">
-        <h3>{currentQuestion}</h3>
+        <h3>abc</h3>
       </div>
-      <div className="app__chat--buttons">
+       <div className="app__chat--buttons">
+        {/* <input type="radio" value={radioButton1Name} name="answer" id="wrongAnswer1"/>{radioButton1Name}
+        <input type="radio" value={radioButton2Name} name="answer" id="wrongAnswer2"/>{radioButton2Name}
+        <input type="radio" value={radioButton3Name} name="answer" id="wrongAnswer3"/>{radioButton3Name}
+        <input type="radio" value={radioButton4Name} name="answer" id="correctAnswer"/>{radioButton4Name} */}
 
-        <input type="radio" value={radioButton1Name} name="answer" /> {radioButton1Name}
-        <input type="radio" value={radioButton2Name} name="answer" /> {radioButton2Name}
-        <input type="radio" value={radioButton3Name} name="answer" /> {radioButton3Name}
-        <input type="radio" value={radioButton4Name} name="answer" /> {radioButton4Name}
-        {/* tutaj zrobic funkcje (onClick), ktora sprawdza, ktory input jest zaznaczony i do tego sie stosuje i zmieni nazwe submit buttona */}
-        <button onClick={() => {check()}}>{submitButtonName}</button>
-
+        <button onClick={() => {check();}}>abc</button>
       </div>
       <div className="app__story">
-        <h3>{}</h3>
+        <h3>abc</h3>
       </div>
     </div>
   );
